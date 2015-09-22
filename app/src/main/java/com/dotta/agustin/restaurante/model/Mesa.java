f27bd19331eac001ad676bff5a0eaa1305c5a51f
Agustín <Agustin@MacBook-Pro-de-Agustin.local>
2015-09-22 20:33:00 +0200

    First commit

---
+++ app/src/main/java/com/dotta/agustin/restaurante/model/Mesa.java
@@ -0,0 +1,50 @@
+package com.dotta.agustin.restaurante.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Agustin on 22/09/2015.
+ */
+public class Mesa implements Serializable{
+    private int mNumeroMesa;
+    private float mCuenta;
+    private ArrayList<Plato> mPlatos;
+
+    public Mesa(int numeroMesa, float cuenta, ArrayList<Plato> platos) {
+        mNumeroMesa = numeroMesa;
+        mCuenta = cuenta;
+        mPlatos = platos;
+    }
+
+    public int getNumeroMesa() {
+        return mNumeroMesa;
+    }
+
+    public void setNumeroMesa(int numeroMesa) {
+        mNumeroMesa = numeroMesa;
+    }
+
+    public float getCuenta() {
+        return mCuenta;
+    }
+
+    public void setCuenta(float cuenta) {
+        mCuenta = cuenta;
+    }
+
+    public ArrayList<Plato> getPlatos() {
+        return mPlatos;
+    }
+
+    public void setPlatos(ArrayList<Plato> platos) {
+        mPlatos = platos;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesa{" +
+                "mNumeroMesa=" + mNumeroMesa +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/dotta/agustin/restaurante/model/Plato.java
@@ -0,0 +1,70 @@
+package com.dotta.agustin.restaurante.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Agustin on 22/09/2015.
+ */
+public class Plato implements Serializable{
+    private String mNombre;
+    private String mImagen;
+    private ArrayList<String> mAlergenos;
+    private int mPrecio;
+    private String mDescripcion;
+
+    public Plato(String nombre, String imagen, ArrayList<String> alergenos, int precio, String descripcion) {
+        mNombre = nombre;
+        mImagen = imagen;
+        mAlergenos = alergenos;
+        mPrecio = precio;
+        mDescripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return mNombre;
+    }
+
+    public void setNombre(String nombre) {
+        mNombre = nombre;
+    }
+
+    public String getImagen() {
+        return mImagen;
+    }
+
+    public void setImagen(String imagen) {
+        mImagen = imagen;
+    }
+
+    public ArrayList<String> getAlergenos() {
+        return mAlergenos;
+    }
+
+    public void setAlergenos(ArrayList<String> alergenos) {
+        mAlergenos = alergenos;
+    }
+
+    public int getPrecio() {
+        return mPrecio;
+    }
+
+    public void setPrecio(int precio) {
+        mPrecio = precio;
+    }
+
+    public String getDescripcion() {
+        return mDescripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        mDescripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Plato{" +
+                "mDescripcion='" + mDescripcion + '\'' +
+                '}';
+    }
+}
